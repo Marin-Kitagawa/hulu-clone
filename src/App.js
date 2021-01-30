2f84d07607dccf3adf5e229a4a0a75015e0563a6
RAMASUBRAMANIYAN G <gitbash.hub@gmail.com>
2021-01-31 01:38:32 +0530

    Firebase Deploy

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
+import React, { useState } from 'react';
 import Header from './Header'
 import Nav from './Nav'
-
+import Results from './Results'
+import requests from './requests'
 function App() {
+  const [selectedOption, setSelectedOption] = useState(requests.fetchTrending);
   return (
     <div className="app">
       <Header />
-      <Nav />
-      {/* Results */}
+      <Nav setSelectedOption={setSelectedOption} />
+      <Results selectedOption={selectedOption} />
     </div>
   );
 }
