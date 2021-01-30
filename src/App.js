18a6f299e248beaf4c5fe5aa52f4282c86839f25
RAMASUBRAMANIYAN G <gitbash.hub@gmail.com>
2021-01-30 21:18:09 +0530

    NavBar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
 import Header from './Header'
+import Nav from './Nav'
 
 function App() {
   return (
     <div className="app">
       <Header />
-
-
-      {/* NavBar */}
+      <Nav />
       {/* Results */}
     </div>
   );
